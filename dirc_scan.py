0287d0d62607ef003aedb1cd72ab4d4bc9948086
Max-codestore <116363309+Max-codestore@users.noreply.github.com>
2022-11-24 15:16:01 +0000

    Add files via upload

---
+++ dirc_scan.py
@@ -0,0 +1,29 @@
+import os,shutil
+
+class scanner:
+    def __init__(self):
+        self.path ='testing'
+        self.file_list = os.listdir(self.path)
+        self.move_to = 'testing2'
+        self.file_extention = 'txt'
+    def select(self):
+        contains = S.filter()
+        print(contains)
+        if len(contains) != 0:
+            file_select = int(input('enter the index of the file you want to move? '))
+            S.movement(contains[file_select])
+        else:
+            print('there are no files here')
+    def movement(self,file):
+        local= self.path +'/'+file
+        shutil.move(local,self.move_to)
+        print('finished')
+    def filter(self):
+        contains = []
+        for i in range(len(self.file_list)):
+            if self.file_extention in self.file_list[i]:
+                contains.append(self.file_list[i])
+        return contains
+
+S = scanner()
+S.select()
