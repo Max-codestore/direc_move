7a64c4f6f4953c041b8f1e2f415e0d79904b1cc9
Max-codestore <116363309+Max-codestore@users.noreply.github.com>
2022-11-25 15:13:19 +0000

    Update dirc_scan.py
    added the ability to copy files instead of just moving them

--- dirc_scan.py
+++ dirc_scan.py
@@ -7,6 +7,7 @@ class scanner:
         self.move_to = 'testing2'
         self.file_extention = '.txt'
         self.repeat = True
+        self.copy = False
     def select(self):
         contains = S.filter()
         if len(contains) != 0:
@@ -20,6 +21,10 @@ class scanner:
         local= self.path +'/'+file
         shutil.move(local,self.move_to)
         print('finished')
+    def copying(self,file):
+        local= self.path +'/'+file
+        shutil.copy(local,self.move_to)
+        print('finished')
     def filter(self):
         contains = []
         for i in range(len(self.file_list)):
@@ -29,17 +34,32 @@ class scanner:
     def printing(self,contains):
         print('enter the index of the file to move')
         print('-----------------------------')
+        if self.copy == True:
+            print('Current Mode-Copy')
+        else:
+            print('Current Mode-Move')
         for i in range(len(contains)):
             print('{0}:{1}'.format(i,contains[i]))
+        if self.copy == True:
+            print('M:move mode')
+        else:
+            print('C:copy mode')
         print('E:exit')
         print('-----------------------------')
         file = input('')
         if file.lower() == 'e':
             self.repeat = False
             return
+        elif file.lower() == 'm':
+            self.copy = False
+        elif file.lower() == 'c':
+            self.copy= True
         else:
             try:
-                S.movement(contains[int(file)])
+                if self.copy == False:
+                    S.movement(contains[int(file)])
+                else:
+                    S.copying(contains[int(file)])
             except:
                 print('sorry, something went wrong when attemping to move file number {0}'.format(file))
 S = scanner()
